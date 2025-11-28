a40ecb43fc64d82afb9b9f87798d9962f78ed2d1
julian-go <mail@juliangottwald.com>
2025-11-28 12:27:08 +0100

    Add 2024/10-2

--- src/solvers/year2024/day10.rs
+++ src/solvers/year2024/day10.rs
@@ -58,11 +58,44 @@ pub fn part1(input: &str) -> Result<String, String> {
             }
         }
     }
-    println!("--------");
     Ok(scores.iter().sum::<i64>().to_string())
+}
+
+fn trailhead_score_distinct(map: &Map<i64>, pos: Pose) -> i64 {
+    let mut queue = VecDeque::new();
+    let mut reachable = Vec::new();
+    queue.push_back(pos);
+    while let Some(current_pos) = queue.pop_front() {
+        let Some(&current_height) = map.get(current_pos) else {
+            continue;
+        };
+
+        current_pos
+            .adjacent()
+            .iter()
+            .filter_map(|&adjacent_pos| map.get(adjacent_pos).map(|&height| (adjacent_pos, height)))
+            .filter(|(_, height)| *height == current_height + 1)
+            .for_each(|(adjacent_pos, height)| {
+                if height == 9 {
+                    reachable.push(adjacent_pos);
+                } else {
+                    queue.push_back(adjacent_pos);
+                }
+            });
+    }
+    reachable.len() as i64
 }
 
 pub fn part2(input: &str) -> Result<String, String> {
     let map = parse(input).map_err(|e| e.to_string())?.1;
-    Ok(1.to_string())
+    let mut scores = Vec::new();
+    for y in 0..map.height() {
+        for x in 0..map.width() {
+            let pos = Pose::new_xy(x as i64, y as i64);
+            if map.get(pos).is_some_and(|v| *v == 0) {
+                scores.push(trailhead_score_distinct(&map, pos));
+            }
+        }
+    }
+    Ok(scores.iter().sum::<i64>().to_string())
 }
